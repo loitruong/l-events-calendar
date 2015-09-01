3595a589445dc7d4ac06275d4686819b499bca1b
Joseph Fusco <hello@josephfus.co>
2015-08-31 22:47:36 -0400

    fix: admin settings page shortcode / styles

--- option-page.php
+++ option-page.php
@@ -14,16 +14,16 @@
     {
          // This page will be under "Settings"
          add_menu_page(
-             'LEC Setting', 
-             'LEC Setting', 
-             'manage_options', 
+             'LEC Setting',
+             'LEC Setting',
+             'manage_options',
              'lec-setting',
              array($this, 'create_admin_page')
              //icon
              //position
          );
     }
-     
+
      /**
       * Options page callback
       */
@@ -34,18 +34,13 @@
       ?>
       <div class="wrap">
           <h2>L Events Calendar</h2>
-          <p>
-           there's no settings at the momment. However, it will be available upon on request.
-          </p>
+          <p>There are no settings at the momment. Please <a href="https://github.com/loitruong/l-events-calendar/issues/new" target="_blank">open a new issue</a> for feature requests.</p>
           <h2>Plugin Instruction</h2>
-          <div>
-            1) put this shortcode in any page
-            <xmp style="color: white; max-width: 100%; background: black; width: 100%; max-width: 500px">
-  [l-events-calendar]
-            </xmp>
-            2) add event inside l-event
-            3) enjoy your coolest event calendar
-          </div>
+          <ol>
+            <li>Use the shortcode <code>[l-event-calendar]</code> on any page</li>
+            <li>Add an event inside l-event</li>
+            <li>Enjoy the coolest event calendar ever.</li>
+          </ol>
       <?php
 
      }
